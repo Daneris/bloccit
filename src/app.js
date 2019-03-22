ae144971466079ab8418ab2f2d83abc4f249bbe4
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-03-22 19:19:48 -0400

    fixing err

--- src/app.js
+++ src/app.js
@@ -2,9 +2,3 @@ const express = require("express");
 const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
-
-
-appConfig.init();
-routeConfig.init(app);
-module.exports = app;
-//comments
