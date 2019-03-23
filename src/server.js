3941b4994a64aaa8d77d058e834ff61710100ab9
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-03-23 12:04:13 -0400

    fixing errors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
 const app = require("./app");
 const http = require("http");
 
+console.log(app);
 const port = normalizePort(process.env.PORT || 3000);
-//app.set("port", port);
+app.set("port", port);
 
 const server = http.createServer(app)
 
