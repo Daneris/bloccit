fb2272d4bf9856f30abaf5ec9d6e28dabc39b796
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-03-26 16:47:45 -0400

    edits

---
+++ src/db/models/rule.js
@@ -0,0 +1,23 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Rule = sequelize.define('Rule', {
+    description: DataTypes.STRING,
+    topicId: {
+      type: DataTypes.INTEGER,
+      onDelete: "CASCADE",
+      references: {
+        model: "Topics",
+        key: "id",
+        as: "topicId",
+      }
+    }
+  }, {});
+  Rule.associate = function(models) {
+    rule.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+
+    })// associations can be defined here
+  };
+  return Rule;
+};
