72a31a677ec100857c33172f8f880f0c469105e2
Daneris Cruz <daneriscruz@Daneriss-MacBook-Air.local>
2019-04-20 20:36:41 -0400

    checkpoint 3004 authentication complete

--- src/db/migrations/20190416203935-create-flair-post.js
+++ src/db/migrations/20190416203935-create-flair-post.js
@@ -1,24 +1,18 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Users', {
+    return queryInterface.createTable('FlairPosts', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: { msg: "must be a valid email" }
-        }
+      flairId: {
+        type: Sequelize.INTEGER
       },
-      password: {
-        type: Sequelize.STRING,
-        allowNull: false
+      postId: {
+        type: Sequelize.INTEGER
       },
       createdAt: {
         allowNull: false,
@@ -31,6 +25,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Users');
+    return queryInterface.dropTable('FlairPosts');
   }
 };
